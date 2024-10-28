8b6288f047c95f2261e962edad8289fb8f8b15f9
Auspicious <info@auspicioussoft.com>
2024-10-28 15:39:54 +0530

    Refactor addClientBilling to return CREATED status code

--- src/controllers/admin/admin.ts
+++ src/controllers/admin/admin.ts
@@ -193,7 +193,7 @@ export const updateClient = async (req: Request, res: Response) => {
 export const addClientBilling = async (req: Request, res: Response) => {
     try {
         const response = await addClientBillingService({ id: req.params.id, ...req.body }, res)
-        return res.status(httpStatusCode.OK).json(response)
+        return res.status(httpStatusCode.CREATED).json(response)
     } catch (error: any) {
         const { code, message } = errorParser(error)
         return res.status(code || httpStatusCode.INTERNAL_SERVER_ERROR).json({ success: false, message: message || "An error occurred" });
