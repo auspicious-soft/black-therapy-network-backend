64482d9de2dd54b0b4e5ffcb13ee28a0342679a4
Auspicious <info@auspicioussoft.com>
2024-11-07 10:02:39 +0530

    Add updateAlert functionality to alerts controller and service

--- src/controllers/alerts/alerts-controllers.ts
+++ src/controllers/alerts/alerts-controllers.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
 import { httpStatusCode } from "../../lib/constant";
 import { errorParser } from "../../lib/errors/error-response-handler";
-import { addAlertService, getAlertsService } from "../../services/alerts/alerts-service";
+import { addAlertService, getAlertsService, updateAlertService } from "../../services/alerts/alerts-service";
 
 export const addAlert = async (req: Request, res: Response) => {
     try {
@@ -18,6 +18,18 @@ export const getAlerts = async (req: Request, res: Response) => {
     try {
         const response = await getAlertsService(req.query, res)
         return res.status(httpStatusCode.OK).json({ success: true, message: "Alerts fetched successfully", data: response })
+    }
+    catch (error: any) {
+        const { code, message } = errorParser(error)
+        return res.status(code || httpStatusCode.INTERNAL_SERVER_ERROR).json({ success: false, message: message || "An error occurred" })
+    }
+}
+
+export const updateAlert = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+        const response = await updateAlertService({ id, ...req.body }, res)
+        return res.status(httpStatusCode.OK).json({ success: true, message: "Alert updated successfully", data: response })
     }
     catch (error: any) {
         const { code, message } = errorParser(error)
--- src/services/alerts/alerts-service.ts
+++ src/services/alerts/alerts-service.ts
@@ -1,9 +1,14 @@
+import { httpStatusCode } from "src/lib/constant"
+import { errorResponseHandler } from "src/lib/errors/error-response-handler"
 import { AlertModel } from "src/models/admin/alerts-schema"
 import { queryBuilder } from "src/utils"
 
 export const addAlertService = async (data: any, res: any) => {
     const response = await AlertModel.create(data)
-    return response
+    return {
+        success: true,
+        data: response
+    }
 }
 
 export const getAlertsService = async (payload: any, res: any) => {
@@ -12,7 +17,7 @@ export const getAlertsService = async (payload: any, res: any) => {
     const { query } = queryBuilder(payload)
     const response = await AlertModel.find(query).skip(offset).limit(limit).populate('userId')
     const totalDataCount = Object.keys(query).length < 1 ? await AlertModel.countDocuments() : await AlertModel.countDocuments(query)
-    if(response.length > 0) {
+    if (response.length > 0) {
         return {
             success: true,
             data: response,
@@ -29,5 +34,15 @@ export const getAlertsService = async (payload: any, res: any) => {
             limit,
             total: 0
         }
+    }
+}
+
+export const updateAlertService = async (payload: any, res: any) => {
+    const alert = await AlertModel.findById(payload.id)
+    if (!alert) return errorResponseHandler('Alert not found', httpStatusCode.NOT_FOUND, res)
+    const response = await AlertModel.findByIdAndUpdate(payload.id, payload, { new: true })
+    return {
+        success: true,
+        data: response
     }
 }
