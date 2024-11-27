0e1efdc1e19fd1a4bf7e959c1ec773bfe90573a2
Auspicious <info@auspicioussoft.com>
2024-11-27 16:20:52 +0530

    Enhance chat history retrieval by populating sender details and handling empty message cases

--- src/routes/chats.ts
+++ src/routes/chats.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { upload } from "../configF/multer";
 import { checkMulter } from "../lib/errors/error-response-handler"
 import { MessageModel } from "../models/chat-message-schema";
+import { onboardingApplicationModel } from "src/models/therapist/onboarding-application-schema";
 
 const router = Router();
 
@@ -12,11 +13,29 @@ router.get('/chat-history/:roomId', async (req, res) => {
         const messages = await MessageModel.find({ roomId }).sort({ createdAt: 1 }).populate('sender')
         // .skip((Number(page) - 1) * Number(limit))
         // .limit(Number(limit))
+        if (messages.length === 0) {
+            res.status(200).json({
+                success: true,
+                message: 'No chat history found',
+                data: []
+            })
+            return
+        }
+
+        const populatedMessages = await Promise.all(messages.map(async (message) => {
+            const therapistDetails = await onboardingApplicationModel.findOne({ therapistId: message.sender._id });
+            if (therapistDetails) {
+                (message as any).sender = therapistDetails
+            }
+            return message
+        }))
+
         res.status(200).json({
             success: true,
             message: 'Chat history fetched successfully',
-            data: messages
+            data: populatedMessages
         })
+        
     } catch (error) {
         console.error('Error fetching chat history:', error);
         res.status(500).json({ error: 'Failed to fetch chat history' });
