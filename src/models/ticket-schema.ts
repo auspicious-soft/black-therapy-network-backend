2e912717de633cc050ebc4d56632baf749e2bc34
Auspicious <info@auspicioussoft.com>
2024-12-03 12:58:10 +0530

    Add status, title, and message fields to ticket schema

--- src/models/ticket-schema.ts
+++ src/models/ticket-schema.ts
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 const ticketSchema = new mongoose.Schema({
     sender: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'clients' },
     roomId: { type: String, required: true , unique: true},
+    status: { type: String, required: true, enum: ['Pending', 'Completed'] },
+    title: { type: String, required: true },
+    message: { type: String, required: true },
 }, {
     timestamps: true
 })
