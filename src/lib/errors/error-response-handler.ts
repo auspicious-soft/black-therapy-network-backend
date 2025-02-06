14a167836b0b950710ed588bad81bf611b83c2b3
Auspicious <info@auspicioussoft.com>
2025-02-06 15:57:29 +0530

    Add console logging for error messages in errorResponseHandler

--- src/lib/errors/error-response-handler.ts
+++ src/lib/errors/error-response-handler.ts
@@ -3,6 +3,7 @@ import { Response } from "express";
 import { httpStatusCode } from "../constant";
 
 export const errorResponseHandler = (message: string, code: number = 500, res: Response) => {
+    console.log('message: ', message);
     throw new Error(JSON.stringify({
         success: false,
         message,
